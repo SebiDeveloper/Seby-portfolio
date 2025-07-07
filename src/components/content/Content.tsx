1c4cec402c5d71422e0a89b13404a27bb5172b71
Sebastian G <seba.ri@mail.ru>
2025-07-07 16:28:24 +0300

    added ts in project

---
+++ src/components/content/Content.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Content.scss";
+
+const Content: React.FC = () => {
+
+    return (
+        <div className="content">
+            <div className="content__header"></div>
+            <div className="content__main"></div>
+        </div>
+    )
+};
+
+export default Content;
